dec925c8e8f08bcefb3060d69c3d154b971ab6ba
Namsik Jung <nsjung74@gmail.com>
2020-07-09 18:21:04 +0900

    python psycopg2 관련

---
+++ 9.10.psycopg2.import.sh
@@ -0,0 +1,10 @@
+
+# psycopg2 
+# ImportError: No module named psycopg2
+
+yum -y install python-pip
+pip -y install --upgrade pip
+yum -y install build-dep python-psycopg2
+pip -y install psycopg2
+
+
